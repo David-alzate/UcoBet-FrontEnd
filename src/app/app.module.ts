8083806d980ded1386ba6413989075a41caaef3b
David-alzate <alzatedavid0126@gmail.com>
2024-10-21 13:49:56 -0500

    Creacion de la estructura del proyecto y componente de register new city

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterCityComponent } from './modules/city/components/register-city/register-city/register-city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterCityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/City/components/Register-City/register-city/register-city.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-city',
+  templateUrl: './register-city.component.html',
+  styleUrls: ['./register-city.component.css']
+})
+export class RegisterCityComponent {
+
+}
